df80f86dfeb81ee852db1f08093db9362e9191ce
Martin Ender <m.buettner.d@gmail.com>
2019-04-06 13:26:15 +0200

    Extract retinate logic into thenable function

--- src/commands.ts
+++ src/commands.ts
@@ -18,58 +18,74 @@ export function runOnActiveDocument(outputChannel: vscode.OutputChannel) {
             }
             const scriptPath = uri[0].fsPath;
 
-            const config = vscode.workspace.getConfiguration('retinate');
-            const timeout = config.get('timeout', 3);
-            const maxBufferSize = config.get('maxOutputSize', 200*1024);
-            const retinaPath = config.get('retinaPath', 'retina');
-
-            let process = child_process.exec(
-                `${retinaPath} "${scriptPath}"`,
-                {
-                    "timeout": timeout * 1000,
-                    "maxBuffer": maxBufferSize
-                },
-                (error, stdout, stderr) => {
-                    if (error) {
-                        if (!error.code && error.signal === 'SIGTERM') {
-                            const msg = 'Retina aborted due to timeout.';
-                            window.showErrorMessage(msg);
-                            outputChannel.appendLine(msg);
-                            outputChannel.appendLine("Retina's output started with:");
-                            outputChannel.appendLine(stdout.substr(0, 1024));
-                        // @ts-ignore
-                        } else if (error.code === 'ERR_CHILD_PROCESS_STDIO_MAXBUFFER') {
-                            const msg = 'Retina aborted due to exceeding maximum output size.';
-                            window.showErrorMessage(msg);
-                            outputChannel.appendLine(msg);
-                            outputChannel.appendLine("Retina's output started with:");
-                            outputChannel.appendLine(stdout.substr(0, 1024));
-                        } else {
-                            window.showErrorMessage(`Retina failed to run with error code ${error.code}. See output window for details.`);
-                            outputChannel.appendLine(error.message);
-                            outputChannel.appendLine(stderr);
-                        }
-                        return;
-                    }
-
+            retinate(scriptPath, editor.document.getText()).then(
+                (result) => {
                     editor.edit((editBuilder: vscode.TextEditorEdit) => {
                         let invalidRange = new vscode.Range(
                             0, 0,
                             editor.document.lineCount /*intentionally missing the '-1' */, 0);
                         let fullRange = editor.document.validateRange(invalidRange);
-                        editBuilder.replace(fullRange, stdout);
+                        editBuilder.replace(fullRange, result);
                     }).then(success => {
                         if (!success) {
                             window.showErrorMessage('It borken');
                         }
                     });
+                },
+                ({message, log}) => {
+                    window.showErrorMessage(message);
+                    outputChannel.appendLine(log);
                 }
             );
-
-            process.stdin.write(editor.document.getText());
-            process.stdin.end();
         });
     } else {
         window.showWarningMessage('No active document.');
     }
+}
+
+function retinate(scriptPath: string, input: string): Thenable<string> {
+    const config = vscode.workspace.getConfiguration('retinate');
+    const timeout = config.get('timeout', 3);
+    const maxBufferSize = config.get('maxOutputSize', 200*1024);
+    const retinaPath = config.get('retinaPath', 'retina');
+
+    let promise = new Promise<string>((resolve, reject) => {
+        let retinaProcess = child_process.exec(
+            `${retinaPath} "${scriptPath}"`,
+            {
+                "timeout": timeout * 1000,
+                "maxBuffer": maxBufferSize
+            },
+            (error, stdout) => {
+                if (error) {
+                    if (!error.code && error.signal === 'SIGTERM') {
+                        const msg = 'Retina aborted due to timeout.';
+                        reject({
+                            'message': msg,
+                            'log': `${msg}\nRetina's output started with:\n${stdout.substr(0, 1024)}`
+                        });
+                    // @ts-ignore
+                    } else if (error.code === 'ERR_CHILD_PROCESS_STDIO_MAXBUFFER') {
+                        const msg = 'Retina aborted due to exceeding maximum output size.';
+                        reject({
+                            'message': msg,
+                            'log': `${msg}\nRetina's output started with:\n${stdout.substr(0, 1024)}`
+                        });
+                    } else {
+                        reject({
+                            'message': `Retina failed to run with error code ${error.code}. See output window for details.`,
+                            'log': error.message
+                        });
+                    }
+                } else {
+                    resolve(stdout);
+                }
+            }
+        );
+    
+        retinaProcess.stdin.write(input);
+        retinaProcess.stdin.end();
+    });
+
+    return promise;
 }
