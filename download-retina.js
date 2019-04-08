26f97c74d57258923b90581aee35a4d6c50ad1d1
Lukas Niemeier <lukas.niemeier@koeln.de>
2019-04-08 11:47:06 +0200

    Simplify retina download

--- download-retina.js
+++ download-retina.js
@@ -2,6 +2,19 @@ const http = require('follow-redirects').https;
 const fs = require('fs');
 const tar = require('tar-fs');
 const gunzip = require('gunzip-maybe');
+const format = require('util').format;
+
+const version = process.env.npm_package_retina_version || 'unknown-version';
+const platforms = Object.keys(process.env)
+    .filter(key => key.startsWith('npm_package_retina_platforms_'))
+    .map(key => process.env[key]) || [];
+
+const template = 'https://github.com/m-ender/retina/releases/download/%s/retina-%s.tar.gz';
+const directory = 'bin';
+
+if (!fs.existsSync(directory)) {
+    fs.mkdirSync(directory);
+}
 
 const download = function (url, destination) {
     const file = fs.createWriteStream(destination);
@@ -9,21 +22,11 @@ const download = function (url, destination) {
         response.pipe(file);
         file.on('finish', function () {
             file.close(() => {
-                fs.createReadStream(destination).pipe(gunzip()).pipe(tar.extract("."));
+                fs.createReadStream(destination).pipe(gunzip()).pipe(tar.extract(directory));
             });
         });
     });
 };
 
-binaries = [{
-    url: 'https://github.com/m-ender/retina/releases/download/v1.1.1/retina-win-x64.tar.gz',
-    name: 'win'
-}, {
-    url: 'https://github.com/m-ender/retina/releases/download/v1.1.1/retina-linux-x64.tar.gz',
-    name: 'linux'
-}];
-
-console.log("Downloading...");
-for (binary of binaries) {
-    download(binary['url'], binary['name']);
-}
+console.log(`Downloading version ${version} for platforms [${platforms}]...`);
+platforms.forEach(platform => download(format(template, version, platform), `${directory}/${platform}.tmp`));
